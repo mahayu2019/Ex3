3b61947351c4895f7cd56a785d167e215abe1052
mahayu2019 <2353987275@qq.com>
2018-11-11 21:31:27 +0800

    添加

--- app/src/main/java/com/onecode/ex3/MainActivity.java
+++ app/src/main/java/com/onecode/ex3/MainActivity.java
@@ -6,12 +6,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         btn.setOnClickListener(this);
     }
 
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                String inputText = editText.getText().toString();
 //                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
                 imageView.setImageResource(R.drawable.apple); //动态载入图片
+//                if (progressBar.getVisibility() == View.GONE) {
+//                    progressBar.setVisibility(View.VISIBLE);
+//                } else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
+                int progress = progressBar.getProgress();
+                progress = progress + 10;
+                progressBar.setProgress(progress);
                 break;
             default:
                 break;
